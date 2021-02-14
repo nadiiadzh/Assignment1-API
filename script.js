962062a8f0e0a544bd07bf28990deab2229262a7
Nadiia Dzhyhryniuk <nadiadzh7@gmail.com>
2021-02-14 00:48:57 -0500

    updated code

--- script.js
+++ script.js
@@ -1,39 +1,173 @@
 document.addEventListener("DOMContentLoaded",function(){
     console.log("works!");
 
-    const factEl = document.getElementById("fact")
-    const numberEl = document.getElementById("number")
+    const factEl = document.getElementById("fact") 
+    const triviaFactEl = document.getElementById("trivia-fact")
+    const mathFactEl = document.getElementById("math-fact")
+    const dateFactEl = document.getElementById("date-fact")
+    const yearFactEl = document.getElementById("year-fact")
+
+    const triviaNumberEl = document.getElementById("trivia-number")
+    const mathNumberEl = document.getElementById("math-number")
+    const yearNumberEl = document.getElementById("year-number")
+    const monthNumberEl = document.getElementById("month-number")
+    const dayNumberEl = document.getElementById("day-number")
+
     const getRandomBtn = document.getElementById("getRandomBtn")
     const getDateFactBtn = document.getElementById("getDateFactBtn")
     const getMathFactBtn = document.getElementById("getMathFactBtn")
     const getTriviaFactBtn = document.getElementById("getTriviaFactBtn")
     const getYearFactBtn = document.getElementById("getYearFactBtn")
 
-    getRandomBtn.addEventListener("click", function(){
-        getFact("year");
-
+    getTriviaFactBtn.addEventListener("click", function(){ // add event listener for the button "get trivia fact"
+        if(triviaNumberEl.value ===""){
+            triviaFactEl.style.color='red';
+            triviaFactEl.innerHTML = `<p>Please enter the number! </p>`;
+           // alert("Please enter the number!")
+        }else{
+            triviaFactEl.style.color='black';
+            getTriviaFact(triviaNumberEl.value); 
+        }
     });
+
+    getMathFactBtn.addEventListener("click", function(){ // add event listener for the button "get math fact"
+
+        if(mathNumberEl.value ===""){
+            mathFactEl.style.color='red';
+            mathFactEl.innerHTML = `<p>Please enter the number! </p>`;
+           // alert("Please enter the number!")
+        }else{
+            mathFactEl.style.color='black';
+            getMathFact(mathNumberEl.value);
+        }
+ 
+     }); 
+
     getDateFactBtn.addEventListener("click", function(){
-        getDateFact("12","1");
+        if(monthNumberEl.value ==="" || dayNumberEl.value ==="" ){
+            dateFactEl.style.color='red';
+            dateFactEl.innerHTML = `<p>Please enter the month and/or the day! </p>`;
+           // alert("Please enter the date!")
+        }else{
+            dateFactEl.style.color='black';
+            getDateFact(monthNumberEl.value, dayNumberEl.value);
+        }
 
     });
-    getMathFactBtn.addEventListener("click", function(){
-        getMathFact("1729");
-
-    });
-    getTriviaFactBtn.addEventListener("click", function(){
-        getTriviaFact("42");
-
-    });
+    
     getYearFactBtn.addEventListener("click", function(){
-        getYearFact("1492");
+
+        if(yearNumberEl.value ===""){
+            yearFactEl.style.color='red';
+            yearFactEl.innerHTML = `<p>Please enter the number! </p>`;
+           // alert("Please enter the year!")
+        }else{
+            yearFactEl.style.color='black';
+            getYearFact(yearNumberEl.value);
+        }
+    });
+
+  
+        
+        const factType = ["trivia", "math", "date", "year"];
+        const random = Math.floor(Math.random() * factType.length);
+        console.log(random, factType[random]);
+    
+    
+
+    getRandomBtn.addEventListener("click", function(){
+        //console.log(random, factType[random]);
+        
+        getFact(factType[random]);
 
     });
+
+    function createTriviaFact(response){ // creates trivia fact  
+        console.log(response);
+        triviaFactEl.innerHTML = `<p> ${response.number} is ${response.text}. </p>`;
+
+    }
+    function createMathFact(response){
+        console.log(response);
+        mathFactEl.innerHTML = `<p> ${response.number} is ${response.text}. </p>`;
+
+    }
+    function createYearFact(response){
+        console.log(response);
+        yearFactEl.innerHTML = `<p> ${response.number} is ${response.text}. </p>`;
+
+    }
+    function createDateFact(response){
+        console.log(response);
+        dateFactEl.innerHTML = `<p>It is ${response.text}. </p>`;
+
+    }
     function createFact(response){
         console.log(response);
-        factEl.innerHTML = `<p> ${response.text} </p>`;
-        numberEl.innerHTML = `<p> ${response.number} </p>`;
+        factEl.innerHTML = `<p> ${response.number} is ${response.text}. </p>`;
+
     }
+
+
+    function getTriviaFact(n){ // function, what gets trivia fact using fetch()  
+
+        fetch(`https://numbersapi.p.rapidapi.com/${n}/trivia?fragment=true&notfound=floor&json=true`, {
+             "method": "GET",
+            "headers": {
+                 "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
+                "x-rapidapi-host": "numbersapi.p.rapidapi.com"
+                }
+            })
+            .then((response) => response.json())
+            .then((response) => createTriviaFact(response)) 
+            .catch(err => {
+                 console.error(err);
+            });
+        } 
+    function getMathFact(num){ // function, what gets math fact using fetch()
+        fetch(`https://numbersapi.p.rapidapi.com/${num}/math?fragment=true&json=true`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
+                "x-rapidapi-host": "numbersapi.p.rapidapi.com"
+            }
+        })
+            .then((response) => response.json())
+            .then((response) => createMathFact(response))
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    function getDateFact(month,day){ // function, what gets date fact using fetch()
+        fetch(`https://numbersapi.p.rapidapi.com/${month}/${day}/date?fragment=true&json=true`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
+                    "x-rapidapi-host": "numbersapi.p.rapidapi.com"
+                }
+            })
+            .then((response) => response.json())
+            .then((response) => createDateFact(response))
+            .catch(err => {
+                console.error(err);
+            });
+    }  
+      
+    function getYearFact(year){
+        fetch(`https://numbersapi.p.rapidapi.com/${year}/year?fragment=true&json=true`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
+                "x-rapidapi-host": "numbersapi.p.rapidapi.com"
+            }
+        })
+    
+            .then((response) => response.json())
+            .then((response) => createYearFact(response))
+            .catch(err => {
+                console.error(err);
+            });
+    } 
 
     function getFact(type){
         fetch(`https://numbersapi.p.rapidapi.com/random/${type}?max=20&fragment=true&min=10&json=true`, {
@@ -51,66 +185,8 @@ document.addEventListener("DOMContentLoaded",function(){
             });
     }
 
-    function getDateFact(month,day){
-        fetch(`https://numbersapi.p.rapidapi.com/${month}/${day}/date?fragment=true&json=true`, {
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
-                    "x-rapidapi-host": "numbersapi.p.rapidapi.com"
-                }
-            })
-            .then((response) => response.json())
-            .then((response) => createFact(response))
-            .catch(err => {
-                console.error(err);
-            });
-    }  
-    function getMathFact(num){
-        fetch(`https://numbersapi.p.rapidapi.com/${num}/math?fragment=true&json=true`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
-                "x-rapidapi-host": "numbersapi.p.rapidapi.com"
-            }
-        })
-            .then((response) => response.json())
-            .then((response) => createFact(response))
-            .catch(err => {
-                console.error(err);
-            });
-    }   
-    function getTriviaFact(n){
-        fetch(`https://numbersapi.p.rapidapi.com/${n}/trivia?fragment=true&notfound=floor&json=true`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
-                "x-rapidapi-host": "numbersapi.p.rapidapi.com"
-            }
-        })
-            .then((response) => response.json())
-            .then((response) => createFact(response))
-            .catch(err => {
-                console.error(err);
-            });
-    }    
-    function getYearFact(year){
-        fetch(`https://numbersapi.p.rapidapi.com/${year}/year?fragment=true&json=true`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "bfedd34141msh8906e4917f64faap1045cfjsnc02d70e6c5a9",
-                "x-rapidapi-host": "numbersapi.p.rapidapi.com"
-            }
-        })
     
-            .then((response) => response.json())
-            .then((response) => createFact(response))
-            .catch(err => {
-                console.error(err);
-            });
-    } 
 
-
-    
    /* getJokeByIdBtn.addEventListener("click", function(){
         getJokeById("2d6c37fc75bc42f383864555f0b8f110");
 
